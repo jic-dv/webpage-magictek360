f1a9a2dc890ecf3ecb560236cadff3837ac7e2c8
jic-dv <jic-dev@outlook.com>
2024-10-01 13:22:36 -0300

    feat: optimize images from .png to .webp format

--- src/components/landing-page.jsx
+++ src/components/landing-page.jsx
@@ -4,7 +4,7 @@ const LandingPage = () => {
       {/* Left Side with Background Image for Larger Screens */}
       <div
         className="hidden lg:flex lg:w-1/2 bg-cover bg-center justify-center items-center p-6"
-        style={{ backgroundImage: "url('/images/bg-globes.svg')" }}
+        style={{ backgroundImage: "url('/images/bg-globes.webp')" }}
       >
         <div className="text-center w-4/6 z-10 mr-0 lg:mr-12">
           <h1
@@ -19,13 +19,13 @@ const LandingPage = () => {
       {/* Right Side (background image for mobile and large screens) */}
       <div
         className="relative flex-1 bg-cover bg-center lg:w-1/2"
-        style={{ backgroundImage: "url('/images/bg-landing-page.png')" }}
+        style={{ backgroundImage: "url('/images/bg-landing-page.webp')" }}
       >
         {/* Overlay for small screens */}
         <div className="absolute inset-0 bg-black opacity-50 lg:hidden"></div>
 
         <img
-          src="/images/bg-landing-page.png"
+          src="/images/bg-landing-page.webp"
           alt="Immersive Magic"
           className="w-full h-full object-cover lg:hidden"
         />
@@ -36,7 +36,7 @@ const LandingPage = () => {
         {/* Circular Logo */}
         <div className="w-44 h-44 lg:w-72 lg:h-72 bg-white rounded-full shadow-lg flex justify-center items-center border-4 border-white">
           <img
-            src="/images/logo-magictek360.png"
+            src="/images/logo-magictek360.webp"
             alt="Magic Tek 360 Logo"
             className="w-full h-full object-cover rounded-full"
           />
